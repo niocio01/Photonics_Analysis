353cc2e7ad9800e3ab7af78d0d7f817ab2bd4a1e
recordar94 <baumgartnermartino@hotmail.com>
2017-12-18 11:42:58 +0100

    FrageSteller hochgeladen

---
+++ src/FragenSteller.h
@@ -0,0 +1,22 @@
+/*B10_StructAdresse.c
+Programm das einen Text auf die Konsole ausgibt 
+Autor: Martino Baumgartner
+Firma: HTW Chur/Photonics 
+Version: 0.2 
+Datum: 12.11.2017
+Änderungen: 
+ */
+
+// Einbinden von Headerdateien der Programmbibliothek. 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#indef FRAGENERFASSEN_H
+#define FRAGENERFASSEN_H
+
+void nextQuestion(frage_t *p_fragen int frageNr);
+
+#endif
